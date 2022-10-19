df91682d433ff91035ba7641e6f715a9de8874fa
Kiril R <kyryl.ryabov@gmail.com>
2022-10-19 19:25:54 +0300

    Added resources

---
+++ resources/model_jwt_attributes.go
@@ -0,0 +1,12 @@
+/*
+ * GENERATED. Do not modify. Your changes might be overwritten!
+ */
+
+package resources
+
+import "time"
+
+type JwtAttributes struct {
+	ExpirationTime time.Time `json:"expiration_time"`
+	Jwt            string    `json:"jwt"`
+}
