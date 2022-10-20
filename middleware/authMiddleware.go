da160ebed30f53c53db3a3e0f9d7abb1e1545b9f
Kiril R <kyryl.ryabov@gmail.com>
2022-10-20 11:12:35 +0300

    Export AuthJWT

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -13,7 +13,7 @@ import (
 func BasicAuth() func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ok, err := authJWT(r)
+			ok, err := AuthJWT(r)
 			if err != nil || !ok {
 				if err == nil {
 					err = errors.New("invalid credentials")
@@ -27,7 +27,7 @@ func BasicAuth() func(next http.Handler) http.Handler {
 	}
 }
 
-func authJWT(r *http.Request) (bool, error) {
+func AuthJWT(r *http.Request) (bool, error) {
 	auth := r.Header.Get("Authorization")
 	spitted := strings.Fields(auth)
 	if len(spitted) > 1 {
